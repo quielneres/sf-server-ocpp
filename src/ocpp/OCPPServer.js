10eb2a346d7e2236e713e5bfb2d01196a63daefa
ezequiel <quielneres@gmail.com>
2025-03-12 14:01:43 -0300

    Força o uso do TLS 1.2

--- src/ocpp/OCPPServer.js
+++ src/ocpp/OCPPServer.js
@@ -10,8 +10,8 @@ class OCPPServer {
 
         // 🔹 Carregar os certificados SSL/TLS
         const options = {
-            minVersion: 'TLSv1.2', // Força o uso do TLS 1.2
-            maxVersion: 'TLSv1.2', // Força o uso do TLS 1.2
+            // minVersion: 'TLSv1.2', // Força o uso do TLS 1.2
+            // maxVersion: 'TLSv1.2', // Força o uso do TLS 1.2
             cert: fs.readFileSync(__dirname + '/certs/fullchain.pem'),
             key: fs.readFileSync(__dirname + '/certs/privkey.pem'),
             ciphers: [
